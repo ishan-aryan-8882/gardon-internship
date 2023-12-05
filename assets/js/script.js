065a2948b0c4a1e692c42d6f95c72698c359bae8
unknown <Devl-Minhaj>
2023-12-05 18:30:18 +0530

    added sliding up in owl-caraousel

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,38 @@
 $(".hero-carousel").owlCarousel({
-    loop: true,
-    items: 1,
-    nav: true,
-    animateOut: 'slideOutUp',
-  animateIn: 'slideInUp',
-    navText: ["<i class='fa fa-long-arrow-left'></i>", "<i class='fa fa-long-arrow-right'></i>"],
-    autoplay: true,
-    autoplayTimeout: 2000, // Adjust the timeout as needed
-  });
-  
-  $(document).ready(function () {
-    function changeImgSrc() {
-        var image = $("#myimage");
-        if ($(window).width() < 770) {
-            image.attr("src", "assets/img/square-cut.png");
-        } else {
-            image.attr("src", "assets/img/complete-white-part-logo.png");
-        }
-    }
-
-    $(window).on("load resize", changeImgSrc);
+  loop: true,
+  autoplay: true,
+  items: 1,
+  nav: true,
+  animateOut: "slideOutUp",
+  animateIn: "slideInUp",
+  navText: [
+    "<i class='fa fa-long-arrow-left'></i>",
+    "<i class='fa fa-long-arrow-right'></i>",
+  ],
+  autoplayTimeout: 2000, // Adjust the timeout as needed
 });
 
-let burgerMenu = document.querySelector('.fa-grip-lines');
-let sideMenu = document.querySelector('.nav-show-on-small-screen');
-let cross = document.querySelector('.closing-cross');
+$(document).ready(function () {
+  function changeImgSrc() {
+    var image = $("#myimage");
+    if ($(window).width() < 770) {
+      image.attr("src", "assets/img/square-cut.png");
+    } else {
+      image.attr("src", "assets/img/complete-white-part-logo.png");
+    }
+  }
 
-burgerMenu.addEventListener('click',()=>{
-    sideMenu.classList.toggle('small-nav-show-right');
-})
+  $(window).on("load resize", changeImgSrc);
+});
 
-cross.addEventListener('click',()=>{
-    sideMenu.classList.remove('small-nav-show-right');
-})
-  
+let burgerMenu = document.querySelector(".fa-grip-lines");
+let sideMenu = document.querySelector(".nav-show-on-small-screen");
+let cross = document.querySelector(".closing-cross");
+
+burgerMenu.addEventListener("click", () => {
+  sideMenu.classList.toggle("small-nav-show-right");
+});
+
+cross.addEventListener("click", () => {
+  sideMenu.classList.remove("small-nav-show-right");
+});
