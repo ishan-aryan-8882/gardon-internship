131e9915bc27ada3f3a288a716303efd039a58a1
unknown <Devl-Minhaj>
2023-12-05 15:38:21 +0530

    added support for rezising at breakpoints and removing the top navigation bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,15 @@ $(".hero-carousel").owlCarousel({
     autoplayTimeout: 2000, // Adjust the timeout as needed
   });
   
+  function changeImgSrc() {
+    var image = document.querySelector("#myimage");
+    if (window.innerWidth < 770) {
+      image.src = "assets/img/square-cut.png";
+    } else {
+      image.src = "assets/img/complete-white-part-logo.png";
+    }
+  }
+  
+  window.addEventListener("load", changeImgSrc);
+  window.addEventListener("resize", changeImgSrc);
+  
