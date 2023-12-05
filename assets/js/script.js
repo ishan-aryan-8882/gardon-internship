d23cab0d7fed3d7198d8cedc559e2adb53959d2a
unknown <Devl-Minhaj>
2023-12-05 18:21:42 +0530

    removed test text

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,8 @@ $(".hero-carousel").owlCarousel({
     loop: true,
     items: 1,
     nav: true,
-    // animateOut: 'slideOutUp',
-    // animateIn: 'slideInUp',
+    animateOut: 'slideOutUp',
+  animateIn: 'slideInUp',
     navText: ["<i class='fa fa-long-arrow-left'></i>", "<i class='fa fa-long-arrow-right'></i>"],
     autoplay: true,
     autoplayTimeout: 2000, // Adjust the timeout as needed
